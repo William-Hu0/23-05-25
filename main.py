074a2b8fb1f79f760cb000b13d6c5c425d054df6
William-Hu0 Hu <hu.william@iisgalvanimi.edu.it>
2025-05-23 17:21:59 +0000

    finito

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from studente import Studente 
+from studente import Studente
+from rettangolo import Rettangolo
 
 studente0=Studente("William","3FINF",[],)
 
@@ -7,3 +8,11 @@ studente0.aggiungi_voto(9)
 studente0.aggiungi_voto(10)
 
 studente0.stampa_info()
+
+ret0=Rettangolo(12,6,"blu")
+ret1=Rettangolo(3,4,"verde")
+ret2=Rettangolo(7,3,"bianco")
+
+ret0.stampa_info()
+ret1.stampa_info()
+ret2.stampa_info()
--- rettangolo.py
+++ rettangolo.py
@@ -1,2 +1,12 @@
 class Rettangolo:
     def __init__(self,base,altezza,colore):
+        self.base=base
+        self.altezza=altezza
+        self.colore=colore
+    def area(self):
+        return self.base*self.altezza
+    def stampa_info(self):
+        print(self.base)
+        print(self.altezza)
+        print(self.colore)
+        print(self.area())    
